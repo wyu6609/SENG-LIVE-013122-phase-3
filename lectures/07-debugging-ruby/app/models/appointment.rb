f81ca4425f4ff2925ee093be0635a7edd98b8d7f
William Yu <wy427@nyu.edu>
2022-03-22 19:57:19 -0400

    Update appointment.rb

--- lectures/07-debugging-ruby/app/models/appointment.rb
+++ lectures/07-debugging-ruby/app/models/appointment.rb
@@ -1,27 +1,38 @@
 class Appointment < ActiveRecord::Base
-    belongs_to :patient
-    belongs_to :vet
+  belongs_to :patient
+  belongs_to :vet
 
-    # Activity 1 => Building Aggregate Class / Instance Methods
+  # Activity 1 => Building Aggregate Class / Instance Methods
 
-    # Class Methods
+  # Class Methods
 
-    # Return earliest appointment
-    def self.earliest
-    end
+  # Return earliest appointment
+  def self.earliest
+    #return entire appointment jinstance
+    #self.order(:date).first
 
-    # Return latest appointment
-    def self.latest
-    end
-    
-    # Instance Methods
-    
-    # Return appointment patient's name
-    def get_patient_name
-    end
+    #return date of earliest appointment Instance
+    self.all.minimum(:date)
+  end
 
-    # Return appointment vet's name
-    def get_vet_name
-    end
-end 
+  # Return latest appointment
+  def self.latest
+    #self.order(:date).latest
+    #self.order(:date).last
+    self.all.maximum(:date)
+  end
 
+  # Instance Methods
+
+  # Return appointment patient's name
+  def get_patient_name
+    binding.pry
+    self.patient.name
+  end
+
+  # Return appointment vet's name
+
+  def get_vet_name
+    self.vet.vet_name
+  end
+end
