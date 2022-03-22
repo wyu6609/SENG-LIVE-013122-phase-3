914e06abf1c939098a3d3b87b307e5fba2e15fdb
William Yu <wy427@nyu.edu>
2022-03-22 19:56:44 -0400

    Update patient.rb

--- lectures/07-debugging-ruby/app/models/patient.rb
+++ lectures/07-debugging-ruby/app/models/patient.rb
@@ -1,52 +1,73 @@
 class Patient < ActiveRecord::Base
-    belongs_to :clinic
+  belongs_to :clinic
 
-    has_many :appointments
-    has_many :vets, through: :appointments
+  has_many :appointments
+  has_many :vets, through: :appointments
 
-    # Activity 2 => Building Aggregate Class / Instance Methods
+  # Activity 2 => Building Aggregate Class / Instance Methods
 
-    # Class Methods
+  # Class Methods
 
-    # Return all patient names
-    def self.all_patient_names
-    end
+  # Return all patient names
+  def self.all_patient_names
+    binding.pry
+    self.all.pluck(:name)
+  end
 
-    # Return all patient species
-    def self.all_patient_species
-    end
+  # Return all patient species
+  def self.all_patient_species
+    binding.pry
+    self.all.pluck(:species)
+  end
 
-    # Return all patient ages
-    def self.all_patient_ages
-    end
+  # Return all patient ages
+  def self.all_patient_ages
+    binding.pry
+    self.all.pluck(:ages)
+  end
 
-    # Return oldest patient
-    def self.oldest
-    end
+  # Return oldest patient
+  def self.oldest
+    self.all.max_by { |p| p.age }
+    #order
+    # self.all.order(:age).last
+  end
 
-    # Return youngest patient
-    def self.youngest
-    end
+  # Return youngest patient
+  def self.youngest
+    binding.pry
+    self.all.min_by { |p| p.age }
+    #order
+    #self.all.order(:age).first
+  end
 
-    # Instance Methods
+  # Instance Methods
 
-    # Return patient's first Vet's name
-    def get_first_vet_name
-    end
-    
-    # Return patient's vet's names
-    def get_vet_names
-    end
+  # Return patient's first Vet's name
+  def get_first_vet_name
+    binding.pry
+    self.vets.first.vet_name
+    #self.vets.pluck(:vet_name).first
+  end
 
-    # Return patient's appointment dates
-    def get_appt_dates
-    end
+  # Return patient's vet's names
+  def get_vet_names
+    self.vets.pluck(:vet_name)
+  end
 
-    # Return patient's number of vets
-    def vet_count
-    end
+  # Return patient's appointment dates
+  def get_appt_dates
+    self.appointments.pluck(:date)
+  end
 
-    # Return patient's number of appointments
-    def appt_count
-    end
-end 
+  # Return patient's number of vets
+  def vet_count
+    self.vets.count
+  end
+
+  # Return patient's number of appointments
+  def appt_count
+    self.appointments.count
+  end
+end
+
